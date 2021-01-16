511f9ccb3b5ac6d5d1f119e53acb6e2faa488ba0
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2021-01-16 22:42:45 +0530

    ✨ adds feature-options ✨ adds react-router-dom as dependency

--- frontend/src/Components/HeroSection/index.js
+++ frontend/src/Components/HeroSection/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import {
   Container,
   GetStarted,
   HeroContainer,
+  Option,
   OptionCard,
   Text,
 } from "./styles";
@@ -60,7 +62,34 @@ class HeroSection extends React.Component {
             Get Started
           </GetStarted>
         </HeroContainer>
-        <OptionCard showOptions={this.state.showOptions}></OptionCard>
+        <OptionCard showOptions={this.state.showOptions}>
+          <Option showOptions={this.state.showOptions}>
+            <h1>Feature 1</h1>
+            <p>
+              lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+            <Link to="/something">Continue</Link>
+          </Option>
+          <Option showOptions={this.state.showOptions}>
+            <h1>Feature 2</h1>
+            <p>
+              lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+            <Link to="/something">Continue</Link>
+          </Option>
+        </OptionCard>
       </Container>
     );
   }
