e31534496718c647ab23d816a0865bbe660f8981
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2021-01-17 02:04:49 +0530

    ✨ elivates HeroSection's state to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -16,22 +17,60 @@ const Container = styled.div`
   }
 `;
 
-function App() {
-  return (
-    <Router>
-      <Container>
-        <Nav />
-        <Switch>
-          <Route path="/" exact>
-            <HeroSection />
-          </Route>
-          <Route path="/recruiter">
-            <Recruiter />
-          </Route>
-        </Switch>
-      </Container>
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      textDisplayed: "",
+      showButton: false,
+      showOptions: false,
+    };
+
+    this.bannerText = "Take a step towards gender inclusivity.";
+  }
+
+  onGetStarted = () => {
+    this.setState({ showButton: false, showOptions: true });
+  };
+
+  updateTextDisplayed = (interval) => {
+    const { textDisplayed } = this.state;
+    const bannerText = this.bannerText;
+
+    if (textDisplayed.length < bannerText.length) {
+      this.setState({
+        textDisplayed: bannerText.slice(0, textDisplayed.length + 1),
+      });
+    } else {
+      clearInterval(interval);
+      if (!this.state.showOptions) this.setState({ showButton: true });
+    }
+  };
+
+  render() {
+    return (
+      <Router>
+        <Container>
+          <Nav />
+          <Switch>
+            <Route path="/" exact>
+              <HeroSection
+                textDisplayed={this.state.textDisplayed}
+                showButton={this.state.showButton}
+                showOptions={this.state.showOptions}
+                updateTextDisplayed={this.updateTextDisplayed}
+                onGetStarted={this.onGetStarted}
+              />
+            </Route>
+            <Route path="/recruiter">
+              <Recruiter />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
