77c32ff8800ed256d94cf0b92bbccd41ee589914
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2021-01-16 15:00:14 +0530

    ☕ adds nav component, & icons ☕ removes booiler plate content

---
+++ frontend/src/Components/Nav/index.js
@@ -0,0 +1,12 @@
+import { Container, Logo } from "./styles";
+
+export const Nav = () => {
+  return (
+    <Container>
+      <Logo>
+        <span>Mark</span>
+        <span>Two</span>
+      </Logo>
+    </Container>
+  );
+};
