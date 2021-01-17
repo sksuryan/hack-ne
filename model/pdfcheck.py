9de3207fe8583b40b60616d110c0766506b32716
vidhigupta9 <gvidhi9@gmail.com>
2021-01-17 19:30:04 +0530

    updated modify function

--- model/pdfcheck.py
+++ model/pdfcheck.py
@@ -2,21 +2,31 @@ import sys, fitz
 import spacy
 import resume_parser
 
-nlp_model = spacy.load('C:/Users/VIDHI/Desktop/Resume Parser/nlp_ner_model')
+
+nlp_model = spacy.load('model/nlp_ner_model')
 
 
-fname = 'Alice Clark CV.pdf'
-doc = fitz.open(fname)
-text = ""
-for page in doc:
+def modify(filepath):
+    fname = "filepath"
+    doc = fitz.open(fname)
+    text = ""
+    for page in doc:
     text = text + str(page.getText())
-tx = " ".join(text.split('\n'))
-#print(tx)
-
-# Applying the model
-doc = nlp_model(tx)
-for ent in doc.ents:
+    tx = " ".join(text.split('\n'))
+    dict = {}
+    # Applying the model
+    doc = nlp_model(tx)
+    for ent in doc.ents:
     print(f'{ent.label_.upper():{30}}- {ent.text}')
+    dict.update({ent.label_.lower():ent.text})
+    changed= tx.replace(dict['name'],"********")
+    name = dict['name']
+    print(changed)
+    f = open('{}.txt'.format(dict['name']), "w")
+    f.write(changed)
+    f.close()
 
-
-
+    #open and read the file after the appending:
+    f = open('{}.txt'.format(dict['name']), "r")
+    print(f.read())
+    f.close()
