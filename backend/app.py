d37aaaaabf5ef6a28de54758145aaca419b8829c
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2021-01-17 17:50:22 +0530

    ☕ adds backend ☕ integrates backend with frontend

---
+++ backend/app.py
@@ -0,0 +1,44 @@
+import os
+from flask import Flask, request, jsonify
+from flask.helpers import send_file, send_from_directory
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+
+app = Flask("__main__")
+CORS(app)
+
+app.config["UPLOAD_FOLDER"] = "uploads/"
+
+if not os.path.exists('uploads'):
+    os.makedirs('uploads')
+
+ALLOWED_EXTENSIONS = set(['pdf'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/recruiter/api', methods=['POST'])
+def upload_file():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		print(request.files)
+		resp = jsonify({'message' : 'No file part in the request'})
+		return resp, 400
+
+	file = request.files['file']
+
+	if file.filename == '':
+		resp = jsonify({'message' : 'No file selected for uploading'})
+		return resp, 400
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		print(os.getcwd())
+		print(os.path.join(os.getcwd(),app.config['UPLOAD_FOLDER']))
+		return send_from_directory(os.path.join(os.getcwd(),app.config['UPLOAD_FOLDER']),filename=filename)
+	else:
+		resp = jsonify({'message' : 'Allowed file types are pdf'})
+		return resp, 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
