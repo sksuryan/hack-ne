fa3a7e83ecbea3adeae353d8a65e607ab704ac14
Vidhi Gupta <42618900+vidhigupta9@users.noreply.github.com>
2021-01-17 20:13:10 +0530

    Indentation fix

--- model/pdfcheck.py
+++ model/pdfcheck.py
@@ -11,14 +11,15 @@ def modify(filepath):
     doc = fitz.open(fname)
     text = ""
     for page in doc:
-    text = text + str(page.getText())
-    tx = " ".join(text.split('\n'))
+        text = text + str(page.getText())
+        tx = " ".join(text.split('\n'))
     dict = {}
     # Applying the model
     doc = nlp_model(tx)
     for ent in doc.ents:
-    print(f'{ent.label_.upper():{30}}- {ent.text}')
-    dict.update({ent.label_.lower():ent.text})
+        print(f'{ent.label_.upper():{30}}- {ent.text}')
+        dict.update({ent.label_.lower():ent.text})
+    
     changed= tx.replace(dict['name'],"********")
     name = dict['name']
     print(changed)
